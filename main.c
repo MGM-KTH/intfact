2e0d4b61c1bd5880648583391e33d523144194da
glindste <gustaflindstedt@gmail.com>
2013-10-25 16:15:44 +0200

    cleaned up read of input

--- main.c
+++ main.c
@@ -18,11 +18,9 @@ int main() {
 int readInput(mpz_t (*array)[100]) {
     mpz_init((*array)[0]);
     int count = 0;
-    mpz_t *ptr = array[0];
     printf("count is %d\n", count);
-    while(count < 100 && gmp_scanf("%Zd", ptr) != EOF) {
+    while(count < 100 && gmp_scanf("%Zd", &((*array)[count])) != EOF) {
         count++;
-        ptr++;
         mpz_init((*array)[count]);
         //printf("count is %d\n", count);
         //printf("array size %lu\n", sizeof(*array)/sizeof((*array)[0]));
