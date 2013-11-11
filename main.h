3a8bb621b381a0c8c4bff38301d0e2c17523dba9
Gustaf Lindstedt <gustaflindstedt@gmail.com>
2013-11-11 01:16:19 +0100

    cleanup

--- main.h
+++ main.h
@@ -12,7 +12,6 @@ int main(int argc, char *argv[]);
 
 void factorize(mpz_t N, mpz_t factors[]);
 void print_factors(mpz_t factors[], int num_factors);
-void next_in_seq(mpz_t next, mpz_t prev, mpz_t N);
 
 int find_trivial_factors(mpz_t N, mpz_t factors[]);
 int fermat(mpz_t N, mpz_t factors[], int num_factors);
